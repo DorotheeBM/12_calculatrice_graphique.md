69978c15da99d2f86e9ec04ac77425e893143975
DorotheeBM <braud.dorothee@gmail.com>
2025-01-31 15:39:08 +0100

    Update script.js
    Utilisation de la fonction eval() qui permet de faire les étapes 3+4+5 en une

--- script.js
+++ script.js
@@ -2,18 +2,47 @@
 const buttons = document.querySelectorAll('button');
 const output = document.getElementById('output');
 
+// Les chiffres et opérateurs s'ajoutent à la suite
+let resetScreen = false;
+
 // Ajout d'un événement de clic sur chaque bouton
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         // Récupère le texte du bouton cliqué
         const currentValue = button.textContent;
         
-        // Si l'affichage est à 0, on remplace par la valeur du bouton
-        if (output.textContent === '0') {
-            output.textContent = currentValue;
-        } else {
-            // Sinon, on ajoute la valeur du bouton à la suite
-            output.textContent += currentValue;
+        // Si c'est le bouton égal, on calcule le résultat
+        if (currentValue === '=') {
+            try {
+                let expression = output.textContent
+                //eval() évalue l'expression mathématique sous forme de texte
+                const result = eval(expression);
+                // Affiche le résultat
+                output.textContent = result;
+                // réinitialiser l'affichage au prochain clic
+                resetScreen = true;
+                // Si Faute de frappe affichage du message "Error"
+            } catch (error) {
+                output.textContent = 'Error';
+                // réinitialiser l'affichage au prochain clic
+                resetScreen = true;
+            }
+        } 
+        // SInon on repart de zéro
+        else {
+            if (resetScreen) {
+                output.textContent = '0';
+                // on peut retaper 
+                resetScreen = false;
+            }
+            // Si 0 est affiché
+            if (output.textContent === '0') {
+                // On affiche le contenu du bouton cliqué
+                output.textContent = currentValue;
+                // sINON on concatène
+            } else {
+                output.textContent += currentValue;
+            }
         }
     });
 });
