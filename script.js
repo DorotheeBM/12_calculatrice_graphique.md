18e65129848dd45fc8f4437e48f876de3b176f2e
DorotheeBM <braud.dorothee@gmail.com>
2025-01-31 15:27:49 +0100

    Update script.js
    ETAPE 2 discard previous commit - affichage débugué (l'autre est un mix avec l'étape suivante)

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 const buttons = document.querySelectorAll('button');
 const output = document.getElementById('output');
 
-// Les chiffres et opérateurs s'ajoutent à la suite
-let resetScreen = false;
-
 // Ajout d'un événement de clic sur chaque bouton
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         // Récupère le texte du bouton cliqué
         const currentValue = button.textContent;
-
+        
+        // Si l'affichage est à 0, on remplace par la valeur du bouton
+        if (output.textContent === '0') {
+            output.textContent = currentValue;
+        } else {
+            // Sinon, on ajoute la valeur du bouton à la suite
+            output.textContent += currentValue;
+        }
     });
 });
