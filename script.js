7ea01f60c382f6816631611f6d7baa45421959c7
DorotheeBM <braud.dorothee@gmail.com>
2025-01-31 15:20:50 +0100

    Update script.js
    ETAPE 2 Affichage du texte du bouton cliqué

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Récupération des boutons et de l'output
+const buttons = document.querySelectorAll('button');
+const output = document.getElementById('output');
 
+// Les chiffres et opérateurs s'ajoutent à la suite
+let resetScreen = false;
+
+// Ajout d'un événement de clic sur chaque bouton
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Récupère le texte du bouton cliqué
+        const currentValue = button.textContent;
+
+    });
+});
